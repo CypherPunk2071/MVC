24bc2e708280078fd825ae07abf8608780df1f86
rabad <rabad@HUSO>
2024-10-13 13:01:46 +0300

    Add project files.

---
+++ BLL/Students.cs
@@ -0,0 +1,12 @@
+﻿#nullable disable
+namespace BLL
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double GPA { get; set; }
+    }
+}
